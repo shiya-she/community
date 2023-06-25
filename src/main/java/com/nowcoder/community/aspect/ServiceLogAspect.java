8f85fc27db622caa325764a7d09ae467bf20ffaf
sc <1713634711@qq.com>
2023-06-25 21:27:26 +0800

    完成统一写入日志

---
+++ src/main/java/com/nowcoder/community/aspect/ServiceLogAspect.java
@@ -0,0 +1,43 @@
+package com.nowcoder.community.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.HttpSessionRequiredException;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author sc
+ * @date 2023-06-25下午 02:31
+ */
+@Component
+@Aspect
+public class ServiceLogAspect {
+    private static final Logger logger = LoggerFactory.getLogger(ServiceLogAspect.class);
+
+    @Pointcut("execution(* com.nowcoder.community.service.*.*(..))")
+    public void pointCut() {
+
+    }
+
+    @Before("pointCut()")
+    public void before(JoinPoint joinPoint) {
+        //用户XXX在yyy访问了[com.newcoder.community.service.xxx()]
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        String ip = request.getRemoteHost();
+        String now = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        String target = joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName();
+        logger.info(String.format("用户[%s],在[%s],访问了[%s].", ip, now, target));
+    }
+}
